9ee61aea38ae940695a437ae11751df1fd4d342f
5yedG1br4n <119901061+5yedG1br4n@users.noreply.github.com>
2024-05-18 17:57:11 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ export default function Header({ fullName, jobProfile }) {
     <header>
       <h1 className="font-Montserrat text-6xl tracking-wider">{fullName}</h1>
       <h2 className="font-Montserrat text-4xl mt-2">{jobProfile}</h2>
-      <HeaderLinks email="5yed.g1br4n@gmail.com" phoneNumber="9146645591" />
+      <HeaderLinks email="5yed.g1br4n@gmail.com" phoneNumber="+91 9146645591" />
     </header>
   );
 }
